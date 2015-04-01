522923e33b1a995f27cba1f5626e61e2a55c9cf7
Keetaek Hong <keetaek@resolutiontube.com>
2015-04-01 12:06:51 -0700

    removing exportProvisioningProfile

--- step.sh
+++ step.sh
@@ -407,8 +407,7 @@ if [[ "${XCODE_BUILDER_ACTION}" == "archive" ]] ; then
       -exportArchive \
       -exportFormat ipa \
       -archivePath "${ARCHIVE_PATH}" \
-      -exportPath "${EXPORT_PATH}" \
-      -exportProvisioningProfile "Resolutiontube-CRM-InHouse-Provision-3-30"
+      -exportPath "${EXPORT_PATH}"
     fail_if_cmd_error "Xcode Export Archive action failed!"
     
     echo_string_to_formatted_output "* Archive build success"
